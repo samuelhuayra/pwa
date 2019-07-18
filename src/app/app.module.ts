a03823688e4799794356c6dd8be4490a3cda6fad
Samuel Huayra <samuel.huayra@salamancasolutions.com>
2019-07-18 02:42:03 -0400

    material and update proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MatToolbarModule, 
+        MatButtonModule, 
+        MatSidenavModule, 
+        MatIconModule, 
+        MatListModule, 
+        MatGridListModule, 
+        MatCardModule, 
+        MatMenuModule, 
+        MatInputModule, 
+        MatSelectModule, 
+        MatSnackBarModule,
+        MatDialogModule,
+        MatRadioModule,
+        MatTabsModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +26,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatSidenavModule, 
+    MatIconModule, 
+    MatListModule, 
+    MatGridListModule, 
+    MatCardModule, 
+    MatMenuModule, 
+    MatInputModule, 
+    MatSelectModule, 
+    MatSnackBarModule,
+    MatDialogModule,
+    MatRadioModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
